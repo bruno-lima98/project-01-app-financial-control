3d79d205836d4d0347a796ee83274d60e1e61bbb
Bruno Lima <bruno.lima@superlogica.com>
2026-04-22 14:25:02 -0300

    method de normalização

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass, field
 from uuid import uuid4
+import unicodedata
 
 # Criando a classe de transação para uso no projeto
 
@@ -15,3 +16,13 @@ class Transacao:
     # Campos com valor padrão (automático ou opicional) devem vir depois: 
     id: str = field(default_factory=lambda: str(uuid4()))
     descricao: str = "" # como esse campo é opicional, precisa de um valor padrão
+
+    # Normalização de entrada dos campos str:
+    @staticmethod # staticmethod significa um método dentro da classe que não depende da instância (self)
+    def normalizar(texto):
+        return unicodedata.normalize("NFD", texto).encode("ascii", "ignore").decode("utf-8").lower().replace(" ", "_")
+    
+    def __post_init__(self):
+        self.tipo = Transacao.normalizar(self.tipo)
+        self.categoria = Transacao.normalizar(self.categoria)
+        self.fonte = Transacao.normalizar(self.fonte)
