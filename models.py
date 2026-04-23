5bd9b096bbf7d56b6485b1041b4703934a72bee2
Bruno Lima <bruno.lima@superlogica.com>
2026-04-23 09:56:20 -0300

    função para deletar transação

--- models.py
+++ models.py
@@ -19,21 +19,29 @@ class Transacao:
     id: str = field(default_factory=lambda: str(uuid4()))
     descricao: str = "" # como esse campo é opicional, precisa de um valor padrão
 
+
     # Normalização de entrada dos campos str:
     @staticmethod # staticmethod significa um método dentro da classe que não depende da instância (self)
     def normalizar(texto):
         return unicodedata.normalize("NFD", texto).encode("ascii", "ignore").decode("utf-8").lower().replace(" ", "_")
     
+    
     # Normalização de entrada do campo date:
     @staticmethod
     def normalizar_data(texto):
         padrao_barra = r"^\d{2}/\d{2}/(?:\d{2}|\d{4})$"
+        padrao_convertido = r"^\d{4}-\d{2}-\d{2}$"
+
         if re.match(padrao_barra, texto):
             return datetime.strptime(texto, "%d/%m/%Y").strftime("%Y-%m-%d")
+        elif re.match(padrao_convertido, texto):
+            return texto
+        
     
     def __post_init__(self):
         self.data = Transacao.normalizar_data(self.data)
         self.tipo = Transacao.normalizar(self.tipo)
         self.categoria = Transacao.normalizar(self.categoria)
         self.fonte = Transacao.normalizar(self.fonte)
+        self.valor = float(self.valor)
 
--- storage.py
+++ storage.py
@@ -2,20 +2,24 @@ import csv
 import os
 from models import Transacao
 
+
+# FUNÇÕES PRINCIPAIS DO APP:
+# 1 - salvar_transacao: salva uma transação nova no banco de dados
+# 2 - carregar_transacoes: retorna todas as transações salvas no banco de dados
+# 3 - deletar_transacoes: seleciona uma transação (via uuid) e remove do banco de dados
+# 4 - atualizar_transacoes:
+
 def salvar_transacao(transacao):
+    # Local onde ficarão os arquivos:
     file_path = "data/data.csv"
-    entrada = [
-                transacao.id,
-                transacao.data,
-                transacao.tipo,
-                transacao.descricao,
-                transacao.categoria,
-                transacao.fonte,
-                transacao.valor
-            ]
+
+    # Lista de entrada do objeto Transacao:
+    entrada = _transacao_para_lista(transacao)
     
+    # Criar a folder caso seja a primeira transação:
     os.makedirs("data", exist_ok=True)
 
+    # Inserir e salvar a transação, verificando se é a primeira transação ou não:
     if os.path.exists(file_path): 
         with open("data/data.csv", "a", newline="", encoding="utf-8") as file:
             writer = csv.writer(file)
@@ -25,3 +29,57 @@ def salvar_transacao(transacao):
             writer = csv.writer(file)
             writer.writerow(["id","data","tipo","descricao","categoria","fonte","valor"])
             writer.writerow(entrada)
+
+
+def carregar_transacoes(arquivo):
+    file_path = f"data/{arquivo}"
+
+    if os.path.exists(file_path): 
+        with open("data/data.csv", "r", encoding="utf-8") as file:
+            reader = csv.reader(file)
+            header = next(reader) # Armazena o cabeçalho e remove dos dados
+            
+            saida = []
+            for row in reader:
+                dic = dict(zip(header,row))
+                saida.append(Transacao(**dic))
+        
+        return saida
+    else:
+        return []
+
+
+def deletar_transacoes(id):
+    file_path = "data/data.csv"
+
+    if not os.path.exists(file_path):
+        return print("Sem arquivo CSV.")
+    else:
+        dados_atual = carregar_transacoes("data.csv")
+        saida = [row for row in dados_atual if row.id != id]
+        salvar_todas(saida)
+
+
+# FUNÇÕES AUXILIARES DO APP:
+# 1 - salvar_todas: reescreve o banco de dados com dados atualizados
+# 2 - _transacao_para_lista: converte uma Transação (objeto) para lista
+
+def salvar_todas(transacoes):
+    file_path = "data/data.csv" 
+    with open(file_path, "w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(["id","data","tipo","descricao","categoria","fonte","valor"])
+        for row in transacoes:
+            writer.writerow(_transacao_para_lista(row))
+
+
+def _transacao_para_lista(transacao):
+    return [
+        transacao.id,
+        transacao.data,
+        transacao.tipo,
+        transacao.descricao,
+        transacao.categoria,
+        transacao.fonte,
+        transacao.valor
+        ]
